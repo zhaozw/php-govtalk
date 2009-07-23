f8889f49cf19fc73656ef7959499f0e878689875
jonathon <jonathon@fubra.com>
2009-07-23 11:45:00 +0000

    Rolling reorganisation to trunk.

--- extensions/CompaniesHouse/CompaniesHouse.php
+++ extensions/CompaniesHouse/CompaniesHouse.php
@@ -19,7 +19,7 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
-require_once('GovTalk.php');
+require_once('../../GovTalk.php');
 
 /**
  * Companies House API client.  Extends the functionality provided by the
