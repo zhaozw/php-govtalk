a01363dac68ba5d6a655710e06b93c6c9ee837dd
jonathon <jonathon@fubra.com>
2009-08-11 17:33:09 +0000

    Adding first experimental stages of VAT declaration requests.

--- extensions/HMRC/VAT.php
+++ extensions/HMRC/VAT.php
@@ -25,11 +25,22 @@
  * supports V2 of the HMRC VAT internet filing system.  The php-govtalk
  * base class needs including externally in order to use this extention.
  *
+ * The IRmark validation scheme is currently not fully supported by this class.
+ *
  * @author Jonathon Wardman
  * @copyright 2009, Fubra Limited
  * @licence http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License
  */
 class HmrcVat extends GovTalk {
+
+ /* General IRenvelope related variables. */
+
+	/**
+	 * VAT number of the company this return is on the behalf of.
+	 *
+	 * @var string
+	 */
+	private $_vatRegNumber;
 
  /* Magic methods. */
 
@@ -40,16 +51,25 @@ class HmrcVat extends GovTalk {
 	 *
 	 * @param string $govTalkSenderId GovTalk sender ID.
 	 * @param string $govTalkPassword GovTalk password.
-	 * @param boolean $vsips Flag indicating if this request should use the test (VSIPS) system.
+	 * @param string $vsips The service to use ('tpvs', 'vsips', or 'live').
 	 */
-	public function __construct($govTalkSenderId, $govTalkPassword, $vsips = false) {
+	public function __construct($govTalkSenderId, $govTalkPassword, $service = 'live') {
 
-		if ($vsips === true) {
-			parent::__construct('https://secure.dev.gateway.gov.uk/submission', $govTalkSenderId, $govTalkPassword);
-			$this->setTestFlag(true);
-		} else {
-			parent::__construct('https://secure.gateway.gov.uk/submission', $govTalkSenderId, $govTalkPassword);
+		switch ($service) {
+			case 'tpvs':
+				parent::__construct('https://www.tpvs.hmrc.gov.uk/HMRC/VATDEC', $govTalkSenderId, $govTalkPassword);
+				$this->setTestFlag(true);
+			break;
+			case 'vsips':
+				parent::__construct('https://secure.dev.gateway.gov.uk/submission', $govTalkSenderId, $govTalkPassword);
+				$this->setTestFlag(true);
+			break;
+			default:
+				parent::__construct('https://secure.gateway.gov.uk/submission', $govTalkSenderId, $govTalkPassword);
+			break;
 		}
+		
+		$this->setSchemaLocation('http://www.govtalk.gov.uk/taxation/vat/vatdeclaration/2/VATDeclarationRequest-v2-1.xsd');
 
 		$this->setMessageAuthentication('clear');
 		$this->addChannelRoute('http://blogs.fubra.com/php-govtalk/extensions/hmrc/vat/', 'php-govtalk HMRC VAT extension', '0.1');
@@ -57,7 +77,123 @@ class HmrcVat extends GovTalk {
 	}
 
  /* Public methods. */
- 
+
+	/**
+	 * Sets the VAT number to be used for this return submission (the number of
+	 * the company for which this return contains data).
+	 *
+	 * @param string $vatNumber The number to use in the submission.
+	 */
+	public function setVatNumber($vatNumber) {
+	
+		if (preg_match('/^(GB)?(\d{9,12})$/', $vatNumber, $vatNumberChunks)) {
+			$this->_vatRegNumber = $vatNumber;
+			$this->addMessageKey('VATRegNo', $vatNumber);
+		} else {
+			return false;
+		}
+	
+	}
+	
+	/**
+	 * Submits a VAT declaration request.
+	 *
+	 * This method supports final returns using the final argument.
+	 *
+	 * @param string $returnPeriod The period ID this return is for (in the format YYYY-MM).
+	 * @param string $senderCapacity The capacity this return is being submitted under (Agent, Trust, Company, etc.).
+	 * @param float $vatOutput VAT due on outputs (box 1).
+	 * @param float $vatECAcq VAT due on EC acquisitions (box 2)..
+	 * @param float $vatReclaimedInput VAT reclaimed on inputs (box 4).
+	 * @param float $netOutput Net sales and outputs (box 6).
+	 * @param float $newInput Net purchases and inputs (box 7).
+	 * @param float $netECSupply Net EC supplies (box 8).
+	 * @param float $netECAcq Net EC acquisitions (box 9).
+	 * @param float $totalVat Total VAT (box 3). If this value is not specified then it will be calculated as box 1 + box 2. May be skipped by passing null.
+	 * @param float $netVat Net VAT (box 5). If this value is not specified then it will be calculated as the absolute difference between box 5 and box 4. May be skipped by passing null.
+	 * @param boolean $finalReturn Flag indicating if this return is a final VAT return.
+	 */
+	public function declarationRequest($returnPeriod, $senderCapacity, $vatOutput, $vatECAcq, $vatReclaimedInput, $netOutput, $netInput, $netECSupply, $netECAcq, $totalVat = null, $netVat = null, $finalReturn = false) {
+	
+		if (preg_match('/^\d{4}-\d{2}$/', $returnPeriod)) { # VAT period
+			$validCapacities = array('Individual', 'Company', 'Agent',
+			                         'Bureau', 'Partnership', 'Trust',
+			                         'Employer', 'Government', 'Acting in Capacity',
+			                         'Other');
+			if (in_array($senderCapacity, $validCapacities)) {
+				if (is_numeric($vatOutput) && is_numeric($vatECAcq) && is_numeric($vatReclaimedInput) && is_numeric($netOutput) && is_numeric($netInput) && is_numeric($netECSupply) && is_numeric($netECAcq)) {
+				
+	 // Build message body...
+					$package = new XMLWriter();
+					$package->openMemory();
+					$package->setIndent(true);
+					$package->startElement('IRenvelope');
+						$package->writeAttribute('xmlns', 'http://www.govtalk.gov.uk/taxation/vat/vatdeclaration/2');
+						$package->startElement('IRheader');
+							$package->startElement('Keys');
+								$package->startElement('Key');
+									$package->writeAttribute('Type', 'VATRegNo');
+									$package->text($this->_vatRegNumber);
+								$package->endElement(); # Key
+							$package->endElement(); # Keys
+							$package->writeElement('PeriodID', $returnPeriod);
+							$package->writeElement('DefaultCurrency', 'GBP');
+//							$package->startElement('IRmark');
+//								$package->writeAttribute('Type', 'generic');
+//								$package->text('IRmark+Token');
+//							$package->endElement(); # IRmark
+							$package->writeElement('Sender', $senderCapacity);
+						$package->endElement(); # IRheader
+						$package->startElement('VATDeclarationRequest');
+							if ($finalReturn === true) {
+								$package->writeAttribute('finalReturn', 'yes');
+							}
+							$package->writeElement('VATDueOnOutputs', sprintf('%.2f', $vatOutput));
+							$package->writeElement('VATDueOnECAcquisitions', sprintf('%.2f', $vatECAcq));
+							if ($totalVat === null) {
+								$totalVat = $vatOutput + $vatECAcq;
+							}
+							$package->writeElement('TotalVAT', sprintf('%.2f', $totalVat));
+							$package->writeElement('VATReclaimedOnInputs', sprintf('%.2f', $vatReclaimedInput));
+							if ($netVat === null) {
+								$netVat = abs($totalVat - $vatReclaimedInput);
+							}
+							if ($netVat < 0) {
+							   return false;
+							}
+							$package->writeElement('NetVAT', sprintf('%.2f', $netVat));
+							$package->writeElement('NetSalesAndOutputs', floor($netOutput));
+							$package->writeElement('NetPurchasesAndInputs', floor($netInput));
+							$package->writeElement('NetECSupplies', floor($netECSupply));
+							$package->writeElement('NetECAcquisitions', floor($netECAcq));
+						$package->endElement(); # VATDeclarationRequest
+					$package->endElement(); # IRenvelope
+
+	 // Generate IRmark and add it to the body...
+					$bodyText = $package->outputMemory();
+//					$irMark = base64_encode($this->_generateIRMark($bodyText));
+//					$bodyText = str_replace('IRmark+Token', $irMark, $bodyText);
+
+					$this->setMessageBody($bodyText);
+					if ($this->sendMessage() && ($this->responseHasErrors() === false)) {
+var_dump($this->getResponseBody());
+					} else {
+var_dump($this->getResponseErrors());
+						return false;
+					}
+					
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	
+	}
+
  /* Private methods. */
  
 	/**
@@ -72,10 +208,10 @@ class HmrcVat extends GovTalk {
 	private function _generateIRMark($xmlString) {
 	
 		if (is_string($xmlString)) {
-			$xmlString = str_replace('<IRmark>IRmark</IRmark>', '', $xmlString, $matchCount);
+			$xmlString = preg_replace('/<(vat:)?IRmark Type="generic">[A-Za-z0-9\/\+=]*<\/(vat:)?IRmark>/', '', $xmlString, -1, $matchCount);
 			if ($matchCount == 1) {
 				$xmlDom = new DOMDocument;
-				$xmlDom->loadXML($xmlString);
+				$xmlDom->loadXML('<Body xmlns="http://www.govtalk.gov.uk/CM/envelope">'."\n".$xmlString.'</Body>');
 				return sha1($xmlDom->documentElement->C14N(), true);
 			} else {
 				return false;
